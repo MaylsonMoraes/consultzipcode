38137477ad5aaa9d6b3e0b65e51af2b7ca26eb02
Maylson <maylsonmoraesd@hotmail.com>
2022-03-08 16:03:16 -0300

    rota e codigo de consulta de cep por meio do cep-promise

--- src/routes/cep-routes.js
+++ src/routes/cep-routes.js
@@ -1,28 +1,22 @@
 const cep = require('cep-promise');
 const express = require('express');
 const router = express.Router();
-const Cep = require('../models/cep');
 
-class CepController {
-    // método do controller que é acessado através de uma rota
-    show(req, res) {
-      // pego o CEP da requisição do client
-      const cepParaConsulta = req.body.cep;
-      // código do cep-promise
-      cep(cepParaConsulta)
-        .then((result) => {
-          // retorno dos dados em caso de sucesso
-          return res.json(result);
-        })
-        .catch((error) => {
-          // retorno do erro caso exista
-          return res.status(400).json(error);
+
+
+router.get('/', async (req, res) => {
+        
+          const cepParaConsulta = req.body.cep;
+          // código do cep-promise
+          cep(cepParaConsulta)
+            .then((result) => {
+              return res.json(result);
+            })
+            .catch((error) => {
+              return res.status(400).json(error);
+            });
         });
-    }
-  }
-  
-  // exportação da classe CepController
-  export default new CepController();
-  module.exports = router;
+
+module.exports = router;
 
 
